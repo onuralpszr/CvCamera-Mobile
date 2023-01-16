86a264dba97896c6f537222a47e6b975a5cc9efc
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-01-16 16:12:59 +0300

    fix: ✨ setupOpenCV script extra zip file stage removed (not needed)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- setupOpenCV.sh
+++ setupOpenCV.sh
@@ -2,7 +2,6 @@
 
 wget -O opencv-4.7.0-android-sdk.zip https://github.com/opencv/opencv/releases/download/4.7.0/opencv-4.7.0-android-sdk.zip
 unzip -qq opencv-4.7.0-android-sdk.zip
-unzip -qq OpenCV4Android.zip
 mv OpenCV-android-sdk opencvsdk470
 sudo sed -i 's/compileSdkVersion 26/compileSdkVersion 33/g' opencvsdk470/sdk/build.gradle
 sudo sed -i 's/targetSdkVersion 26/targetSdkVersion 33/g' opencvsdk470/sdk/build.gradle
