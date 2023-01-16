d72723fc87f1d390f2f195ac7894a51778e5bbd2
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-01-16 21:14:05 +0300

    feat: 🛠  cvFilters canny filter added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app/src/main/java/com/os/cvCamera/CvFilters.kt
+++ app/src/main/java/com/os/cvCamera/CvFilters.kt
@@ -13,19 +13,28 @@ fun Mat.toSobel(): Mat {
     return this
 }
 
+fun Mat.toSepia(): Mat {
+
+    // multipying image with special sepia matrix
+    val mSepiaKernel = Mat(4, 4, CvType.CV_32F)
+    mSepiaKernel.put(0, 0,  /* R */0.189, 0.769, 0.393, 0.0)
+    mSepiaKernel.put(1, 0,  /* G */0.168, 0.686, 0.349, 0.0)
+    mSepiaKernel.put(2, 0,  /* B */0.131, 0.534, 0.272, 0.0)
+    mSepiaKernel.put(3, 0,  /* A */0.000, 0.000, 0.000, 1.0)
+
+    val tmpMat = Mat()
+    Core.transform(this, tmpMat, mSepiaKernel)
+    return tmpMat
+}
+
 fun CameraBridgeViewBase.CvCameraViewFrame.toSobel(inputMat: Mat): Mat {
     Imgproc.Sobel(this.gray(), inputMat, CV_8UC1, 1, 0);
     return inputMat
 }
 
-fun Mat.toSepia(): Mat {
-    var mSepiaKernel = Mat(4, 4, CvType.CV_32F)
-    mSepiaKernel.put(0, 0,  /* R */0.189, 0.769, 0.393, 0.0)
-    mSepiaKernel.put(1, 0,  /* G */0.168, 0.686, 0.349, 0.0)
-    mSepiaKernel.put(2, 0,  /* B */0.131, 0.534, 0.272, 0.0)
-    mSepiaKernel.put(3, 0,  /* A */0.000, 0.000, 0.000, 1.0)
-    // multipying image with special sepia matrix
-    val tmpMat: Mat = Mat()
-    Core.transform(this, tmpMat, mSepiaKernel)
-    return tmpMat
+fun CameraBridgeViewBase.CvCameraViewFrame.toCanny(inputMat: Mat): Mat {
+    Imgproc.Canny(this.rgba(), inputMat, 80.0, 90.0);
+    return inputMat
 }
+
+
