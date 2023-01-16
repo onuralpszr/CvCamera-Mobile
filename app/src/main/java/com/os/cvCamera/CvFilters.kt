695c854f1bf8856a02e31f2fdb6b81ebf5f4fe92
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-01-16 20:36:20 +0300

    feat: 🛠  cvFilters sobel filter from frame added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app/src/main/java/com/os/cvCamera/CvFilters.kt
+++ app/src/main/java/com/os/cvCamera/CvFilters.kt
@@ -1,5 +1,6 @@
 package com.os.cvCamera
 
+import org.opencv.android.CameraBridgeViewBase
 import org.opencv.core.CvType.CV_8UC1
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
@@ -8,4 +9,9 @@ import org.opencv.imgproc.Imgproc
 fun Mat.toSobel(): Mat {
     Imgproc.Sobel(this, this, CV_8UC1, 1, 0)
     return this
+}
+
+fun CameraBridgeViewBase.CvCameraViewFrame.toSobel(inputMat:Mat):Mat {
+    Imgproc.Sobel(this.gray(),inputMat,CV_8UC1,1,0);
+    return inputMat
 }
