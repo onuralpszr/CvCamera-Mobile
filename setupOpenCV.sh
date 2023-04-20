6adcd1e32834baec7cca21673cc0cf3f3e605bb0
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-04-21 02:07:12 +0300

    feat: agp 8.0 and opencv patch added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- setupOpenCV.sh
+++ setupOpenCV.sh
@@ -3,5 +3,7 @@
 wget -O opencv-4.7.0-android-sdk.zip https://github.com/opencv/opencv/releases/download/4.7.0/opencv-4.7.0-android-sdk.zip
 unzip -qq opencv-4.7.0-android-sdk.zip
 mv OpenCV-android-sdk opencvsdk470
-sudo sed -i 's/compileSdkVersion 26/compileSdkVersion 33/g' opencvsdk470/sdk/build.gradle
-sudo sed -i 's/targetSdkVersion 26/targetSdkVersion 33/g' opencvsdk470/sdk/build.gradle
+
+# Apply OpenCV Build.gradle patch
+sudo patch opencvsdk470/sdk/build.gradle patches/cv_build_gradle.patch
+
