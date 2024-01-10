4e664e30269274dcc6498bf11578833068b08225
Onuralp SEZER <thunderbirdtr@gmail.com>
2024-01-10 20:15:01 +0300

    feat: 🚀 opencv 4.9.0 upgrade and BaseLoaderCallback removed
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- app/src/androidTest/java/com/os/cvCamera/OpenCVLoadInstrumentedTest.kt
+++ app/src/androidTest/java/com/os/cvCamera/OpenCVLoadInstrumentedTest.kt
@@ -4,9 +4,7 @@ import android.view.View
 import androidx.test.core.app.ActivityScenario.ActivityAction
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
@@ -15,7 +13,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.OpenCVLoader.OPENCV_VERSION
 
 
@@ -57,24 +54,6 @@ class OpenCVLoadInstrumentedTest {
         // Context of the app under test.
         val appContext = getInstrumentation().targetContext
         assertEquals("com.os.cvCamera", appContext.packageName)
-    }
-
-    @Test
-    fun opencv_isLoaded() {
-        val appContext = getInstrumentation().targetContext
-        object : BaseLoaderCallback(appContext) {
-            override fun onManagerConnected(status: Int) {
-                when (status) {
-                    SUCCESS -> {
-                        assertEquals(SUCCESS, status)
-                    }
-
-                    else -> {
-                        assertEquals(SUCCESS, status)
-                    }
-                }
-            }
-        }
     }
 
     // Check for toast message context on button click
